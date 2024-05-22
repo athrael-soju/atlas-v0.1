fbc8f095e4efb4bf3c7412cc0225b4b5558ac1e8
Athrael Soju <athrael.soju@gmail.com>
2024-05-22 16:43:00 +0100

    refactor: Adjust input field styling and add close button in header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -1,21 +1,17 @@
 import Link from 'next/link';
 import Image from 'next/image';
 
-import {
-  IconGitHub,
-  IconSeparator,
-  IconSparkles,
-} from '@/components/ui/icons';
+import { IconGitHub, IconSeparator, IconSparkles } from '@/components/ui/icons';
 import { Button } from '@/components/ui/button';
 
 export async function Header() {
   return (
     <header className="sticky top-0 z-50 flex items-center justify-between w-full px-4 border-b h-14 shrink-0 bg-background backdrop-blur-xl">
       <span className="inline-flex items-center home-links whitespace-nowrap">
-        <a 
+        <a
         // href="https://vercel.com" rel="noopener" target="_blank"
         >
-          <Image src="/atlas.png" alt="Atlas Icon" width={50} height={50}  />
+          <Image src="/atlas.png" alt="Atlas Icon" width={50} height={50} />
         </a>
         <IconSeparator className="w-6 h-6 text-muted-foreground/20" />
         <Link href="/">
