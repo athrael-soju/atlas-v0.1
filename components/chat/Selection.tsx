60714219110f806b904346950ab4aed5e4ac73bd
Athrael Soju <athrael.soju@gmail.com>
2024-05-27 17:27:32 +0100

    refactor: Improve chat view with Selection component

--- components/chat/Selection.tsx
+++ components/chat/Selection.tsx
@@ -6,13 +6,26 @@ interface SelectionProps {
 }
 
 export const Selection: React.FC<SelectionProps> = ({ setView }) => (
-  <div className="flex flex-col items-center justify-center h-screen space-y-4">
-    <Button onClick={() => setView('upload')}>
-      Enhance knowledgebase (Forge)
-    </Button>
-    <Button onClick={() => setView('chat')}>
-      Search Knowledgebase (Oracle)
-    </Button>
-    <Button onClick={() => setView('analyze')}>Analyze data (Sage)</Button>
+  <div className="flex items-center justify-center h-screen p-4">
+    <div className="flex flex-col md:flex-row w-full max-w-4x1">
+      <Button
+        onClick={() => setView('upload')}
+        className="flex-1 py-10 md:py-20 text-lg md:text-xl rounded-t-lg md:rounded-l-lg md:rounded-r-none border-b-2 md:border-b-0 md:border-r-2 border-gray-300"
+      >
+        Enhance knowledgebase (Forge)
+      </Button>
+      <Button
+        onClick={() => setView('chat')}
+        className="flex-1 py-10 md:py-20 text-lg md:text-xl border-b-2 md:border-b-0 md:border-r-2 border-gray-300"
+      >
+        Search Knowledgebase (Oracle)
+      </Button>
+      <Button
+        onClick={() => setView('analyze')}
+        className="flex-1 py-10 md:py-20 text-lg md:text-xl rounded-b-lg md:rounded-r-lg md:rounded-l-none"
+      >
+        Analyze data (Sage)
+      </Button>
+    </div>
   </div>
 );
