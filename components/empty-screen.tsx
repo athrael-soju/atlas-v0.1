f344f903df696072c227905c9094f211326324a9
Athrael Soju <athrael.soju@gmail.com>
2024-05-28 15:11:28 +0100

    refactor: Update empty-screen.tsx to remove financial advice note

--- components/empty-screen.tsx
+++ components/empty-screen.tsx
@@ -26,8 +26,7 @@ export function EmptyScreen() {
         </p>
       </div>
       <p className="leading-normal text-muted-foreground text-[0.8rem] text-center max-w-96 ml-auto mr-auto">
-        Note: Data and latency are simulated for illustrative purposes and
-        should not be considered as financial advice.
+        Note: Data and latency are simulated for illustrative purposes.
       </p>
     </div>
   );
