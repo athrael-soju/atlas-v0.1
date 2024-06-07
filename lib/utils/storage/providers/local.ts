3fcd2d9df74e6588653d8d96143396932bacd4e3
Athrael Soju <athrael.soju@gmail.com>
2024-06-07 23:28:48 +0100

    refactor: Improve error handling in Forge, Scribe, and Sage routes

--- lib/utils/storage/providers/local.ts
+++ lib/utils/storage/providers/local.ts
@@ -8,21 +8,25 @@ export async function writeFile(
   file: File,
   userEmail: string
 ): Promise<FileEntry> {
-  const path = join(tmpdir(), file.name);
-  const arrayBuffer = await file.arrayBuffer();
-  const buffer = Buffer.from(arrayBuffer);
-  await fs.writeFile(path, buffer);
+  try {
+    const path = join(tmpdir(), file.name);
+    const arrayBuffer = await file.arrayBuffer();
+    const buffer = Buffer.from(arrayBuffer);
+    await fs.writeFile(path, buffer);
 
-  const response: FileEntry = {
-    id: randomUUID(),
-    userEmail: userEmail,
-    name: file.name,
-    path: path,
-    uploadDate: Date.now(),
-    contentType: file.type,
-  };
+    const response: FileEntry = {
+      id: randomUUID(),
+      userEmail: userEmail,
+      name: file.name,
+      path: path,
+      uploadDate: Date.now(),
+      contentType: file.type,
+    };
 
-  return response;
+    return response;
+  } catch (error: any) {
+    throw new Error('Failed to write file', error.message);
+  }
 }
 
 export async function deleteFile(
@@ -30,6 +34,12 @@ export async function deleteFile(
   userEmail: string
 ): Promise<void> {
   if (file.userEmail !== userEmail) {
-    throw new Error('Unauthorized');
+    throw new Error('Unauthorized to delete file');
+  }
+
+  try {
+    await fs.unlink(file.path);
+  } catch (error: any) {
+    throw new Error('Failed to delete file', error.message);
   }
 }
--- lib/utils/storage/providers/openai.ts
+++ lib/utils/storage/providers/openai.ts
@@ -1,11 +1,27 @@
 import { openai } from '@/lib/client/openai';
 
-export async function uploadDocumentToOpenAi(file: File, userEmail: string) {
-  // TODO: Add a store to keep track of uploaded files, based on userEmail
-  const response = await openai.files.create({
-    file: file,
-    purpose: 'assistants',
-  });
+interface FileUploadResponse {
+  id: string;
+  object: string;
+  bytes: number;
+  created_at: number;
+  filename: string;
+  purpose: string;
+}
 
-  return response;
+// TODO: Add a store to keep track of uploaded files, based on userEmail
+export async function uploadDocumentToOpenAi(
+  file: File,
+  userEmail: string
+): Promise<FileUploadResponse> {
+  try {
+    const response = await openai.files.create({
+      file: file,
+      purpose: 'assistants',
+    });
+
+    return response;
+  } catch (error: any) {
+    throw new Error('Failed to upload document to OpenAI', error.message);
+  }
 }
