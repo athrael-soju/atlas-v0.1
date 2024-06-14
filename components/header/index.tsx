315dd5f2938767dfa6b173e6e5bb7daeb37f487c
Athrael Soju <athrael.soju@gmail.com>
2024-06-14 23:26:07 +0100

    refactor: Update global CSS to animate background gradient
    This commit updates the global CSS file to include a new keyframe animation called "gradientMoveRandom". This animation creates a dynamic gradient effect by changing the background position at different intervals. The animation is applied to the body and header elements, using a linear gradient with two colors. The animation duration is set to 10 seconds and it repeats infinitely.
    
    The purpose of this change is to add visual interest and movement to the background gradient, enhancing the overall user experience.

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -20,7 +20,7 @@ export function Header() {
   };
 
   return (
-    <header className="bg-secondary">
+    <header className="bg-background header">
       <nav
         className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
         aria-label="Global"
