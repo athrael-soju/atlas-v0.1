fa0e8869e005b55ad01dd09301e96fe37ae45cd2
Athrael Soju <athrael.soju@gmail.com>
2024-06-20 16:14:56 +0100

    Update empty screen to display selected assistant in italics

--- components/empty-screen.tsx
+++ components/empty-screen.tsx
@@ -12,7 +12,7 @@ export function EmptyScreen({
     <div className="mx-auto max-w-2xl px-4">
       <div className="flex flex-col gap-2 rounded-2xl bg-secondary sm:p-4 p-2 text-sm sm:text-base">
         <h1 className="text-2xl sm:text-3xl tracking-tight font-semibold max-w-fit inline-block mx-auto text-center">
-          You are now consulting the {assistantSelected}
+          You are now consulting the <u>{assistantSelected}</u>
         </h1>
       </div>
       <p className="leading-normal text-muted-foreground text-[0.8rem] text-center max-w-96 ml-auto mr-auto">
