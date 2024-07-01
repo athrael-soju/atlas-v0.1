e009b61fa43e05731c8023cc483853bef65db133
Athrael Soju <athrael.soju@gmail.com>
2024-07-01 17:45:30 +0100

    refactor: Update .gitignore to ignore all files in .api/__pycache__ directory

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+# Clean up
+rm -rf .next && rm -rf api/__pycache__
+
+# Install Python
+if ! command -v python3 &> /dev/null; then
+  sudo apt-get update
+  sudo apt-get install python3 python3-pip -y
+fi
+
+# Install Python dependencies
+pip3 install -r requirements.txt
+
+# Build Next.js app
+pnpm install
