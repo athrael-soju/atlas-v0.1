b238edfb0e168c812985eb77bbf14e4057d67aaf
Athrael Soju <athrael.soju@gmail.com>
2024-07-01 16:08:32 +0100

    refactor: Update API routes to include '/app' prefix

--- api/index.py
+++ api/index.py
@@ -1,7 +1,42 @@
+from typing import Union
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
+from pydantic import BaseModel
+app = FastAPI()
 
-app = FastAPI(docs_url="/api/py/docs", openapi_url="/api/py/openapi.json")
 
-@app.get("/api/py/healthcheck")
-def healthchecker():
-    return {"status": "success", "message": "Integrated FastAPI Framework with Next.js successfully!"}
+class Item(BaseModel):
+    name: str
+    description: str = None
+
+
+@app.get("/")
+def read_root():
+    return {"Root!"}
+
+
+@app.get("/api/hello")
+def read_x():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+
+@app.post("/items/")
+def create_item(item: Item):
+    return item
+
+
+origins = ['http://127.0.0.1:8000/']
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
