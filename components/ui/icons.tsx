43f243f7232643919d26c44139f3806b6175d040
Athrael Soju <athrael.soju@gmail.com>
2024-07-29 00:30:36 +0100

    refactor: Update NextAuth route to handle provider fallback

--- components/ui/icons.tsx
+++ components/ui/icons.tsx
@@ -338,6 +338,47 @@ function IconChevronDown({ className, ...props }: React.ComponentProps<'svg'>) {
   );
 }
 
+function LoadingIcon({ className, ...props }: React.ComponentProps<'svg'>) {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className="animate-spin"
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <path d="M12 3a9 9 0 1 0 9 9" />
+    </svg>
+  );
+}
+
+function EnterIcon({ className, ...props }: React.ComponentProps<'svg'>) {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <path d="M18 6v6a3 3 0 0 1 -3 3h-10l4 -4m0 8l-4 -4" />
+    </svg>
+  );
+}
+
 export {
   IconAI,
   IconVercel,
@@ -360,4 +401,6 @@ export {
   IconChevronUpDown,
   IconSparkles,
   IconChevronRight,
+  LoadingIcon,
+  EnterIcon,
 };
--- next.config.mjs
+++ next.config.mjs
@@ -82,3 +82,31 @@ export default defineNextConfig({
     ];
   },
 });
+
+async function copyFiles() {
+  try {
+    await fs.access('public/');
+  } catch {
+    await fs.mkdir('public/', { recursive: true });
+  }
+
+  const wasmFiles = (
+    await fs.readdir('node_modules/onnxruntime-web/dist/')
+  ).filter((file) => path.extname(file) === '.wasm');
+
+  await Promise.all([
+    fs.copyFile(
+      'node_modules/@ricky0123/vad-web/dist/vad.worklet.bundle.min.js',
+      'public/vad.worklet.bundle.min.js'
+    ),
+    fs.copyFile(
+      'node_modules/@ricky0123/vad-web/dist/silero_vad.onnx',
+      'public/silero_vad.onnx'
+    ),
+    ...wasmFiles.map((file) =>
+      fs.copyFile(`node_modules/onnxruntime-web/dist/${file}`, `public/${file}`)
+    ),
+  ]);
+}
+
+copyFiles();
